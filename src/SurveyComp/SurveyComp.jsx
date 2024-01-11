0bfbfb4e6f3e9783087886910ca6b3c7584fd63e
Serge <sergesoroka@gmail.com>
2024-01-11 12:29:00 +0200

    Save Incomplete Survey on LocalStorage

--- src/SurveyComp/SurveyComp.jsx
+++ src/SurveyComp/SurveyComp.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
@@ -8,6 +7,31 @@ import { json } from "./json";
 
 function SurveyComponent() {
   const survey = new Model(json);
+
+  const storageItemKey = "my-survey";
+
+  function saveSurveyData(survey) {
+    const data = survey.data;
+    data.pageNo = survey.currentPageNo;
+    window.localStorage.setItem(storageItemKey, JSON.stringify(data));
+  }
+
+  survey.onValueChanged.add(saveSurveyData);
+  survey.onCurrentPageChanged.add(saveSurveyData);
+
+  const prevData = window.localStorage.getItem(storageItemKey) || null;
+  if (prevData) {
+    const data = JSON.parse(prevData);
+    survey.data = data;
+    if (data.pageNo) {
+      survey.currentPageNo = data.pageNo;
+    }
+  }
+
+  survey.onComplete.add(() => {
+    window.localStorage.setItem(storageItemKey, "");
+  });
+
   survey.applyTheme(themeJson);
   survey.onComplete.add((sender, options) => {
     console.log(JSON.stringify(sender.data, null, 3));
