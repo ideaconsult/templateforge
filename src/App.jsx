8f571fc983836b5d89f262fac4008f72ad4a1ef2
Serge <sergesoroka@gmail.com>
2024-02-11 18:59:57 +0200

    basename: designer

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,19 @@
-import { AnimatePresence, motion } from "framer-motion";
-import formatHighlight from "json-format-highlight";
-import { useState } from "react";
 import "./App.css";
-import SurveyComponent from "./SurveyComp/SurveyComp";
-import TopMenuBar from "./MenuBar/TopMenuBar";
-import DialogComp from "./DialogComp/DialogComp";
-import LogoBar from "./MenuBar/LogoBar";
-import StartScreenComp from "./StartScreenComp/StartScreenComp";
-
-import { useShowStartScreen } from "./store/store";
-
-const customColorOptions = {
-  keyColor: "#3a3a3a",
-  numberColor: "blue",
-  stringColor: "red",
-  trueColor: "#00cc00",
-  falseColor: "#ff8080",
-  nullColor: "cornflowerblue",
-};
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [saveSurvey, setSaveSurvey] = useState(false);
-  const [showPreview, setShowPreview] = useState(false);
-  const [result, setResult] = useState(null);
-  const [surveyReset, setSurveyReset] = useState(false);
-  const [templateURL, setTemplateURL] = useState("");
-
-  const [data, setData] = useState(null);
-
-  const startScreen = useShowStartScreen();
-
   return (
-    <>
-      {startScreen ? (
-        <StartScreenComp
-          setSurveyReset={setSurveyReset}
-          surveyReset={surveyReset}
-          setTemplateURL={setTemplateURL}
-          setData={setData}
-        />
-      ) : (
-        <div className="wrapper">
-          <div className="headerWrap">
-            <LogoBar startScreen={false} />
-            <TopMenuBar
-              setSurveyReset={setSurveyReset}
-              setTemplateURL={setTemplateURL}
-            />
-          </div>
-          <div className="mainWrap">
-            <DialogComp result={result} />
-            <SurveyComponent
-              setResult={setResult}
-              surveyReset={surveyReset}
-              data={data}
-            />
-
-            {showPreview && (
-              <AnimatePresence>
-                <motion.div
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  className="portalWrapper"
-                >
-                  <div className="previewModal">
-                    <p
-                      className="modalJson"
-                      dangerouslySetInnerHTML={{
-                        __html: formatHighlight(
-                          result && "<pre>\n" + result + "\n</pre>",
-                          customColorOptions
-                        ),
-                      }}
-                    />
-                    {/* {result} */}
-                  </div>
-                </motion.div>
-              </AnimatePresence>
-            )}
-          </div>
-        </div>
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<HomePage />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NoPage />} />  */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,28 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import TemplatePage from "./pages/TemplatePage.tsx";
-import App from "./App.jsx";
 
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: HomePage,
-    loader: () => fetch(`https://api.ramanchada.ideaconsult.net/template`),
-  },
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: HomePage,
+      loader: () => fetch(`https://api.ramanchada.ideaconsult.net/template`),
+    },
 
-  {
-    path: "/template/:templateId",
-    Component: TemplatePage,
-    loader: ({ params }) =>
-      fetch(
-        `https://api.ramanchada.ideaconsult.net/template/${params.templateId}`
-      ),
-  },
-]);
+    {
+      path: "/template/:templateId",
+      Component: TemplatePage,
+      loader: ({ params }) =>
+        fetch(
+          `https://api.ramanchada.ideaconsult.net/template/${params.templateId}`
+        ),
+    },
+  ],
+  { basename: "/designer" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
