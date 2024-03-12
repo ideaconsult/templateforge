0ad2c9c08a0c9c57d1dad9741e37a1a62e0b9072
Serge <sergesoroka@gmail.com>
2024-03-12 09:24:19 +0200

    Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
     },
   },
 });
