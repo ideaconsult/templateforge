f544daa087df89203b43ca5d8339d82ab0403fdd
Serge <sergesoroka@gmail.com>
2024-05-21 15:54:44 +0300

    Select project test

---
+++ test/select-project.spec.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+const { Builder, Browser, By, Key } = require("selenium-webdriver");
+const { argv } = require("node:process");
+
+describe("Testing of Selecting Project Functionality", async () => {
+  let driver;
+
+  console.log(argv[5] != null ? argv[5] : "CHROME");
+
+  before(async function () {
+    if (argv[5] === "CHROME") {
+      driver = await new Builder().forBrowser(Browser.CHROME).build();
+    }
+    if (argv[5] === "FIREFOX") {
+      driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+    }
+    if (argv[5] === "EDGE") {
+      driver = await new Builder().forBrowser(Browser.EDGE).build();
+    }
+    if (argv[5] == null) {
+      driver = await new Builder().forBrowser(Browser.CHROME).build();
+    }
+  });
+
+  // 1. Search for project
+  it("Search for template", async () => {
+    await driver.get("https://enm-dev.adma.ai/designer");
+    await driver.manage().window().maximize();
+    await driver.findElement(By.id("preferences")).click();
+    //       .sendKeys("test", Key.RETURN);
+    //     const template = await driver.findElement(By.className("nonSelected"));
+    //     await driver.actions().scroll(0, 0, 0, 0, template).perform();
+    //     template.click();
+    //     await driver.findElement(By.id("View")).click();
+    await driver.sleep(5000);
+  });
+});
