a76441fe001f977db1524f97d389de196ad5f4dc
sergesoroka <sergesoroka@gmail.com>
2024-12-09 13:08:28 +0200

    The First Test, trying Cypress

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+  },
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
