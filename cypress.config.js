8eaaebf1e5dc62338356de9a1944c2ceeff859b7
sergesoroka <sergesoroka@gmail.com>
2024-12-21 11:41:12 +0200

    Top Menu Bar Tests

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,18 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        if (browser.name === "chrome") {
+          launchOptions.args.push("--disable-web-security");
+          launchOptions.args.push(
+            "--disable-features=IsolateOrigins,site-per-process"
+          );
+        }
+        return launchOptions;
+      });
     },
   },
+  fixturesFolder: "cypress/fixtures",
 
   component: {
     devServer: {
--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -1,19 +1,51 @@
-const testURLRoot = 'http://127.0.0.1:50722/templates/'
+const testURLRoot = "http://localhost:5173/templates";
 
-describe('Basic site functionality', () => {
+describe("Basic site functionality", () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/template",
+        hostname: "api-test.ramanchada.ideaconsult.net",
+      },
+      {
+        fixture: "json/bk_rcapi_template_default.json",
+      }
+    ).as("getAllTemplates");
 
-  it('displays the under development notice', () => {
-    cy.intercept({
-      method: 'GET',
-      url: '/template',
-      hostname: 'api-test.ramanchada.ideaconsult.net',
-    }, {
-      fixture: 'json/bk_rcapi_template_default.json',
-    })
-    cy.visit(testURLRoot)
+    cy.visit(testURLRoot);
+  });
+  it("displays the under development notice", () => {
     cy.get("#root > div > p")
-      .should('have.class', 'underDev')
-      .and('be.visible')
-  })
+      .should("have.class", "underDev")
+      .and("be.visible");
+  });
 
-})
+  it("Choosing Draft Templates", () => {
+    cy.contains("Drafts Blueprints").click();
+    cy.wait(1000);
+    cy.get(".nonSelected td").eq(1).click();
+    cy.wait(1000);
+    cy.contains("Edit blueprint").click();
+
+    cy.intercept("GET", "/template/*", {
+      fixture: "json/bk_rcapi_template_default.json",
+    }).as("getTemplate");
+
+    cy.wait("@getTemplate")
+      .its("response.statusCode")
+      .should("be.oneOf", [200, 304]);
+
+    cy.get("#Save").click();
+
+    cy.intercept("POST", "/template/*", {
+      fixture: "json/bk_rcapi_template_default.json",
+    }).as("saveTemplate");
+
+    cy.wait("@saveTemplate", { timeout: 10000 }).should(({ request }) => {
+      expect(request.body).to.include("template_name");
+      expect(request.body).to.include("template_author");
+      expect(request.body).to.include("template_acknowledgment");
+    });
+  });
+});
