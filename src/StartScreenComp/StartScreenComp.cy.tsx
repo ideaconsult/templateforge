eba33bbb731450bf30fa2739f1e7e22eb13f2bb6
sergesoroka <sergesoroka@gmail.com>
2025-01-10 14:54:19 +0200

    Component tests temporarily deleted

--- src/StartScreenComp/StartScreenComp.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import StartScreenComp from "./StartScreenComp";
-
-describe("<StartScreenComp />", () => {
-  it("renders", () => {
-    // @ts-ignore
-    cy.mount(<StartScreenComp />);
-  });
-});
