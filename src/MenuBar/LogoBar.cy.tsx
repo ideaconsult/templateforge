76a018caa799e27306c57163b32053a815cf79c1
sergesoroka <sergesoroka@gmail.com>
2025-01-10 14:54:19 +0200

    Component tests temporarily deleted

--- src/MenuBar/LogoBar.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LogoBar from "./LogoBar";
-
-describe("<LogoBar />", () => {
-  it("renders", () => {
-    // @ts-ignore
-    cy.mount(<LogoBar startScreen={true} setProjectID="" uuid="" />);
-  });
-});
