c677aef9403a8ddc802d405c364c5964046cfbec
sergesoroka <sergesoroka@gmail.com>
2025-01-16 13:16:07 +0200

    Search Template test adjustment

--- cypress/e2e/searchForTemplate.cy.js
+++ cypress/e2e/searchForTemplate.cy.js
@@ -2,6 +2,7 @@ const testURLRoot = "http://127.0.0.1:50722/templates/";
 
 describe("Searching for the template", () => {
   beforeEach(() => {
+    cy.visit(testURLRoot);
     cy.intercept(
       {
         method: "GET",
@@ -12,12 +13,10 @@ describe("Searching for the template", () => {
         fixture: "json/bk_rcapi_template_default.json",
       }
     ).as("getAllTemplates");
-
-    cy.visit(testURLRoot);
   });
 
   it("Find template by searching", () => {
-    cy.get(".search").click().type("Dose response");
+    cy.get(".search").click().type("hts_metadata_test_finalized");
     cy.get(".nonSelected td").eq(1).click();
   });
 });
