7968884729413acb61261c057e351786417f0b43
sergesoroka <sergesoroka@gmail.com>
2025-02-06 12:24:32 +0200

    Selecting Project in Generate Excel Test

--- cypress/e2e/generateExcel.cy.js
+++ cypress/e2e/generateExcel.cy.js
@@ -1,6 +1,7 @@
 const testURLRoot = "http://127.0.0.1:50722/templates/";
 const fixture = "json/templates_generated.json";
 const uuid = "396fc51e-dd55-451f-a6dd-913177abb667";
+const project = "enanomapper";
 
 describe("Generate Excel File", () => {
   beforeEach(() => {
@@ -15,14 +16,30 @@ describe("Generate Excel File", () => {
         fixture,
       }
     ).as("getAllTemplates");
+
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/api/projects.json",
+        hostname: "enanomapper.adma.ai",
+      },
+      {
+        fixture: "json/bk_rcapi_projects_default.json",
+      }
+    ).as("getAllProjects");
   });
 
   it("can generate excel file", () => {
     cy.intercept({
       method: "GET",
-      url: `/template/${uuid}?format=xlsx&project=null`,
+      url: `/template/${uuid}?format=xlsx&project=${project}`,
       hostname: "api-test.ramanchada.ideaconsult.net",
     }).as("generateExcelFile");
+
+    cy.get('[data-cy="preferences-btn"]').click();
+    cy.get('[data-cy="select-btn"]').click();
+    cy.get(`[data-project='${project}']`).click();
+    cy.get('[data-cy="ok-btn"]').click();
 
     cy.get('[data-cy="draft"]').click();
     cy.get(".nonSelected td").eq(1).click();
