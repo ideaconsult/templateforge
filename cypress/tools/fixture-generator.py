bee6a5b3fc1e8c905712e6b7f9ea5f5836d63702
Luchesar ILIEV <luchesar.iliev@gmail.com>
2025-02-20 12:00:12 +0200

    template generator: black formatting

--- cypress/tools/fixture-generator.py
+++ cypress/tools/fixture-generator.py
@@ -5,13 +5,20 @@ import uuid
 from datetime import datetime, timedelta
 from pathlib import Path
 
+
 def random_string(length=8):
-    return ''.join(random.choices(string.printable, k=length))
+    return "".join(random.choices(string.printable, k=length))
+
 
 def random_underscores_string(length=8):
-    return ''.join(random_string(length) 
-                   + "_" + random_string(length) 
-                   + "_" + random_string(length))
+    return "".join(
+        random_string(length)
+        + "_"
+        + random_string(length)
+        + "_"
+        + random_string(length)
+    )
+
 
 def random_date(start, end):
     delta = end - start
@@ -19,19 +26,20 @@ def random_date(start, end):
     random_us = random.randint(1, 86400000000)
     return start + timedelta(days=random_days, microseconds=random_us)
 
+
 start_date = datetime(2023, 7, 1)
 end_date = datetime(2024, 7, 1)
 
 data = []
 
 for _ in range(150):
-  date = random_date(start_date, end_date)
+    date = random_date(start_date, end_date)
 
-  random_status = random.choice(['DRAFT', 'FINALIZED'])
+    random_status = random.choice(["DRAFT", "FINALIZED"])
 
-  uuid_value = str(uuid.uuid4())
+    uuid_value = str(uuid.uuid4())
 
-  template = {
+    template = {
         "uri": f"https://api-test.ramanchada.ideaconsult.net/template/{uuid_value}",
         "uuid": uuid_value,
         "METHOD": random_string(12),
@@ -41,9 +49,9 @@ for _ in range(150):
         "template_name": random_string(42),
         "template_status": random_status,
         "template_author": random_string(42),
-        "template_acknowledgment": random_string(34)
-  }
-  data.append(template)
+        "template_acknowledgment": random_string(34),
+    }
+    data.append(template)
 
 dir_name = "cypress/fixtures/json"
 file_name = "bk_rcapi_templates_generated.json"
